ceb91610e9dcb4184ee3b46b3ed25f07b1e3467c
Marcos di Silva <mdsmus@gmail.com>
2008-11-11 15:39:02 -0300

    Métodos para matriz de comparação e elimina outras funções

--- literatura/morris.lisp
+++ literatura/morris.lisp
@@ -1,22 +1,15 @@
 (in-package #:goiaba)
 
-(defun matriz-de-comparacao (classe-de-contorno)
-  "Retorna a matriz de comparacao de um contorno a partir de seu
-classe-de-contorno."
-  (loop
-     for y in classe-de-contorno
-     collect (loop
-                for x in classe-de-contorno
-                collect (positivo-ou-negativo (- x y)))))
+(defmethod matriz-de-comparacao ((objeto contorno-simples))
+  "Gera a matriz de comparação de um contorno em codificação simples."
+  (make-contorno-simples
+   (loop for y in (pontos objeto)
+      collect (loop for x in (pontos objeto)
+                 collect (positivo-ou-negativo (- x y))))))
 
-(defun inverte-matriz-de-comparacao (matriz-de-comparacao)
-  "Retorna a inversão da matriz de comparação de um contorno."
-  (loop
-     for linha in matriz-de-comparacao
-     collect (loop
-                  for elemento in linha
-                  collect (* -1 elemento))))
-
-(defun contour-reduction-algorithm (classe-de-contorno)
-  (let ((contorno-com-duracao (contorno-simples->contorno-com-duracao classe-de-contorno)))
-    (maior-altura-contorno contorno-com-duracao)))
+(defmethod matriz-de-comparacao ((objeto contorno-duracao))
+  "Gera a matriz de comparação de um contorno em codificação simples."
+  (make-contorno-simples
+   (loop for y in (pontos (converter objeto))
+      collect (loop for x in (pontos (converter objeto))
+                 collect (positivo-ou-negativo (- x y))))))
