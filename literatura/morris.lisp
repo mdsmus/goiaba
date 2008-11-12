043b264a7f1b180f4072c912af668c054644a654
Pedro Kroger <pedro.kroger@gmail.com>
2008-11-12 19:18:13 -0300

    imprime matriz de comparacao
    une metodo para matriz-de-comparacao

--- literatura/morris.lisp
+++ literatura/morris.lisp
@@ -1,15 +1,23 @@
 (in-package #:goiaba)
 
-(defmethod matriz-de-comparacao ((objeto contorno-simples))
-  "Gera a matriz de comparação de um contorno em codificação simples."
-  (make-contorno-simples
-   (loop for y in (pontos objeto)
-      collect (loop for x in (pontos objeto)
-                 collect (positivo-ou-negativo (- x y))))))
+(defun %matriz (list)
+  (loop for y in list
+        collect (loop for x in list
+                      collect (positivo-ou-negativo (- x y)))))
 
-(defmethod matriz-de-comparacao ((objeto contorno-duracao))
+(defmethod matriz-de-comparacao ((objeto contorno))
   "Gera a matriz de comparação de um contorno em codificação simples."
-  (make-contorno-simples
-   (loop for y in (pontos (converter objeto))
-      collect (loop for x in (pontos (converter objeto))
-                 collect (positivo-ou-negativo (- x y))))))
+  (let ((fn (typecase objeto
+              (contorno-simples #'identity)
+              (contorno-duracao #'converter)
+              (t (error "nao conheço objeto ~a" objeto)))))
+    (make-contorno-simples (%matriz (pontos (funcall fn objeto))))))
+
+(defmethod print-matriz-de-comparacao ((objeto contorno))
+    (let ((contorno (pontos objeto))
+          (matriz (pontos (matriz-de-comparacao objeto))))
+      (format t "     ~{~2d ~}~%" contorno)
+      (format t "~a~%" (repeat-string (+ 6 (* 2 (1+ (length contorno)))) "-"))
+      (loop for c in contorno
+            for m in matriz do
+            (format t "~2d | ~{~2d ~}~%" c m))))
