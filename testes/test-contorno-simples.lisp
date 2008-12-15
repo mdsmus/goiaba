44eda3f8c7fef19af2abb46be443084791e0bf43
Marcos di Silva <mdsmus@gmail.com>
2008-12-15 09:02:44 -0300

    Insere mais testes simples

--- testes/test-contorno-simples.lisp
+++ testes/test-contorno-simples.lisp
@@ -1,8 +1,18 @@
 (in-package #:goiaba-testes)
 
 (define-test converter
-    (assert-equal #s(3 2 6) (converter #d(#p(0 3) #p(1 2) #p(4 6))))
-  (assert-equal #s(3 2 6) (converter #d(#p(1 2) #p(0 3) #p(4 6)))))
+  (assert-equal #s(3 2 6) (converter #d(#p(0 3) #p(1 2) #p(4 6))))
+  (assert-equal #s(3 2 6) (converter #d(#p(1 2) #p(0 3) #p(4 6))))
+  (assert-equal #d(#p(0 11) #p(1 3) #p(2 5) #p(3 4)) (converter #s(11 3 5 4)))
+  (assert-equal #d(#p(0 1) #p(1 0) #p(2 1) #p(3 0) #p(4 1)) (converter #s(1 0 1 0 1))))
+
+(define-test menor-altura
+  (assert-equal 1 (menor-altura #s(2 3 1)))
+  (assert-equal -4 (menor-altura #s(2 3 -4 1))))
+
+(define-test maior-altura
+  (assert-equal 3 (maior-altura #s(2 3 1)))
+  (assert-equal 1 (maior-altura #s(0 1 0))))
 
 (define-test retrogradar
   (assert-equal #s(3 2 1) (retrogradar #s(1 2 3))))
